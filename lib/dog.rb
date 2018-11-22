77ea173aefd77f7f049fa5e700cb5ea1da2b6c78
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-11-22 21:55:53 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,65 @@
+require "pry"
+
+def reload!
+  load "./lib/dog.rb"
+end
+
+class Dog
+
+  attr_accessor :id, :name, :breed
+
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
+
+      DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS dogs
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+
+    self
+  end
+
+  def self.create(hash)
+    dog = self.new(hash)
+    dog.save
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, id).map do |row|
+      self.new(:id => row[0], :name => row[1], :breed => row[2])
+    end.first
+  end
+
+end
