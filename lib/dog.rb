5785e4f319ea05601957ecf14dc8ba797e7c44df
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-11-22 21:58:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -79,4 +79,13 @@ class Dog
     new_dog
   end
 
+  def self.new_from_db(row)
+    attributes = {
+    :id => row[0],
+    :name => row[1],
+    :breed => row[2]
+    }
+    self.new(attributes)
+  end
+
 end
